dee9dcdb43607ad1c20cbba4f218a4e146bc0da1
Piyapa Dechfoong <59856773+pangpang-1234@users.noreply.github.com>
2022-12-09 13:30:26 +0700

    Add files via upload

--- airflowgcp2.py
+++ airflowgcp2.py
@@ -39,7 +39,7 @@ with DAG(
         # Create bucket
         create_bucket = GCSCreateBucketOperator(
             task_id='create_bucket',
-            bucket_name = 'first_dataset_btctousd',
+            bucket_name = BUCKET_NAME,
             storage_class = 'REGIONAL', # for data availability
             location='asia-southeast1'
         )
@@ -49,18 +49,18 @@ with DAG(
         
         # Get data from GCS > transform > load to GBQ
         transform_file = PythonOperator(
-            task_id='transform',
+            task_id='transform_data',
             python_callable=transform,
             op_kwargs={'BUCKET_NAME':BUCKET_NAME, 'STORAGE_CLIENT':STORAGE_CLIENT, 'BIGQUERY_CLIENT':BIGQUERY_CLIENT,'DATASET_NAME':DATASET_NAME}
         )
         # Create dataset for partitioned tables
-        partition_dataset = BigQueryCreateEmptyDatasetOperator(task_id='partition_dataset', dataset_id=PARTITION_DATASET)
+        partition_dataset = BigQueryCreateEmptyDatasetOperator(task_id='create_partition_dataset', dataset_id=PARTITION_DATASET)
         
         # Create partition tables
         partition_table = PythonOperator(
-            task_id='partition',
+            task_id='create_partition_tables',
             python_callable=create_partition_table,
-            op_kwargs={'BIGQUERY_CLIENT':BIGQUERY_CLIENT, 'PARTITION_DATASET':PARTITION_DATASET, 'DATASET_NAME':DATASET_NAME}
+            op_kwargs={'BIGQUERY_CLIENT':BIGQUERY_CLIENT, 'PARTITION_DATASET':PARTITION_DATASET, 'DATASET_NAME':DATASET_NAME, 'PROJECT_ID':PROJECT_ID}
         ) 
         
         start >> create_bucket >> create_dataset >> transform_file >> partition_dataset >> partition_table
--- function.py
+++ function.py
@@ -6,12 +6,15 @@ import os
 from io import BytesIO
 
 # Get key object from airflow variables
-PROJECT_ID = os.environ.get("PROJECT_ID")
+# PROJECT_ID = os.environ.get("PROJECT_ID")
 JSON_KEY = os.environ.get("KEY")
+PROJECT_ID = "ageless-math-370705"
 
 # Create connection
 STORAGE_CLIENT = storage.Client(JSON_KEY)
 BIGQUERY_CLIENT = bigquery.Client(JSON_KEY)
+# STORAGE_CLIENT = '1'
+# BIGQUERY_CLIENT = '2'
 
 # Define bucket and dataset name
 BUCKET_NAME = 'first_dataset_btctousd'
@@ -19,31 +22,95 @@ DATASET_NAME = 'dataset'
 PARTITION_DATASET = 'partitioned_tables'
 
 # Get data from GCS > transform > load to GBQ
-def transform(BUCKET_NAME,STORAGE_CLIENT,BIGQUERY_CLIENT, DATASET_NAME): # 
-    bucket = STORAGE_CLIENT.get_bucket(BUCKET_NAME) # Create connection to GCS
-    filenames = list(bucket.list_blobs(prefix='')) # List all objects in blob
-    filename  = filenames[0].name # Select first filename
+def transform(BUCKET_NAME,STORAGE_CLIENT,BIGQUERY_CLIENT, DATASET_NAME): 
+    # Create connection to GCS
+    bucket = STORAGE_CLIENT.get_bucket(BUCKET_NAME)
+    
+    # List all objects in blob 
+    filenames = list(bucket.list_blobs(prefix='')) 
+    
+    # Select first filename
+    filename  = filenames[0].name 
     blop = bucket.blob(filename)
-    data = blop.download_as_string() # Download data from blob
-    df = pd.read_csv(io.BytesIO(data), encoding='utf-8',sep=',') # Convert to dataframe 
-    df['Average'] = (df['Open'] + df['High'] + df['Low'])/3 # Add column Average that calculate average from 3 Open, High, Low columns
-    df['VolumeDivClose'] = df['Volume']/df['Close'] # Volume divide by Close
-    df['Min'] = df.min(axis=1) # Find min value each day
-    df['Max'] = df.max(axis=1) # Find max value each day
-    table = f'{DATASET_NAME}.BTCUSD' # Define table name
-    df = df.rename(columns={'Adj Close':'AdjClose'}) # Rename column Adj Close to AdjClose
-    df['Date'] = df['Date'].astype('datetime64[ns]') # Cast column Date to date type
-    job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField('Date', 'DATE')]) # Create schema
-    job = BIGQUERY_CLIENT.load_table_from_dataframe(df, table, job_config=job_config) # Load dataframe to GBQ
+    
+    # Download data from blob
+    data = blop.download_as_string() 
+    
+    # Convert to dataframe
+    df = pd.read_csv(io.BytesIO(data), encoding='utf-8',sep=',')
+    
+    # Add column Average that calculate average from 3 Open, High, Low columns  
+    df['Average'] = (df['Open'] + df['High'] + df['Low'])/3 
+    
+    # Volume divide by Close
+    df['VolumeDivClose'] = df['Volume']/df['Close'] 
+    
+    # Find min value each day
+    df['Min'] = df.min(axis=1)
+    
+     # Find max value each day 
+    df['Max'] = df.max(axis=1)
+    
+    # Define table name
+    table = f'{DATASET_NAME}.BTCUSD' 
+    
+    # Rename column Adj Close to AdjClose
+    df = df.rename(columns={'Adj Close':'AdjClose'}) 
+    
+    # Cast column Date to date type
+    df['Date'] = df['Date'].astype('datetime64[ns]') 
+    
+    # Create schema
+    job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField('Date', 'DATE')])
+    
+    # Load dataframe to GBQ 
+    job = BIGQUERY_CLIENT.load_table_from_dataframe(df, table, job_config=job_config) 
     
 # Create partition tables from GBQ data
-def create_partition_table(BIGQUERY_CLIENT, PARTITION_DATASET, DATASET_NAME):
-    query_month = f"""SELECT DISTINCT EXTRACT(MONTH FROM Date) as M from {DATASET_NAME}.BTCUSD""" # SQL statement that query unique month
-    months = BIGQUERY_CLIENT.query(query_month).to_dataframe()['M'].tolist() # Create unique month list
-    for month in months: # loop in month list
-        query_df = f"""SELECT * from {DATASET_NAME}.BTCUSD 
-                        WHERE EXTRACT(MONTH FROM Date) = {month}""" # SQL statement taht query data by month
-        partitioned_df = BIGQUERY_CLIENT.query(query_df).to_dataframe() # Query data using above sql statement and cast to dataframe
-        table = f'{PARTITION_DATASET}.partitioned_month_{month}' # Define table name
-        job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField('Date', 'DATE')]) # Create schema
-        job = BIGQUERY_CLIENT.load_table_from_dataframe(partitioned_df, table, job_config=job_config) # Upload dataframe to GBQ
+def create_partition_table(BIGQUERY_CLIENT, PARTITION_DATASET, DATASET_NAME, PROJECT_ID):
+    PROJECT_ID = str(PROJECT_ID)
+    # SQL statement that query data by month
+    query_month = f"""SELECT DISTINCT EXTRACT(MONTH FROM Date) as M from {DATASET_NAME}.BTCUSD""" 
+    
+    # Define source_id
+    source_id = f"{PROJECT_ID}.{DATASET_NAME}.BTCUSD"
+    
+    # # Create unique month list
+    months = BIGQUERY_CLIENT.query(query_month).to_dataframe()['M'].tolist() 
+    
+    # loop in month list
+    for month in months:
+        view_id = f"{PROJECT_ID}.{PARTITION_DATASET}.partitioned_month_{month}"
+        view = bigquery.Table(view_id)
+        # Query partition view
+        view.view_query = f"SELECT * FROM `{source_id}` WHERE EXTRACT(MONTH FROM Date) = {month}"
+        # Create view
+        view = BIGQUERY_CLIENT.create_table(view)
+    
+# def create_partition_table(BIGQUERY_CLIENT, PARTITION_DATASET, DATASET_NAME):
+#     # SQL statement that query unique month
+#     query_month = f"""SELECT DISTINCT EXTRACT(MONTH FROM Date) as M from {DATASET_NAME}.BTCUSD""" 
+    
+#     # Create unique month list
+#     months = BIGQUERY_CLIENT.query(query_month).to_dataframe()['M'].tolist() 
+    
+#     # loop in month list
+#     for month in months: 
+#         query_df = f"""SELECT * from {DATASET_NAME}.BTCUSD 
+#                         WHERE EXTRACT(MONTH FROM Date) = {month}""" 
+                        
+#         # Query data using above sql statement and cast to dataframe
+#         partitioned_df = BIGQUERY_CLIENT.query(query_df).to_dataframe() 
+        
+#         # Define table name
+#         table = f'{PARTITION_DATASET}.partitioned_month_{month}' 
+        
+#         # Create schema
+#         job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField('Date', 'DATE')]) 
+        
+#         # Upload dataframe to GBQ
+#         job = BIGQUERY_CLIENT.load_table_from_dataframe(partitioned_df, table, job_config=job_config) 
+
+
+
+# create_partition_table(BIGQUERY_CLIENT, PARTITION_DATASET, DATASET_NAME, PROJECT_ID)
